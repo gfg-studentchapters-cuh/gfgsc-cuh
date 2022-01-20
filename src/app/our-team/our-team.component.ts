727c24b9ed284be7375c5389452647e3c5df5937
gfg-studentchapters-cuh <cuh.gfgsc@gmail.com>
2022-01-20 18:07:44 +0530

    our-team page

--- src/app/our-team/our-team.component.ts
+++ src/app/our-team/our-team.component.ts
@@ -10,96 +10,96 @@ export class OurTeamComponent implements OnInit {
 
   ngOnInit(): void {}
 
-  serviceBlock = [
-    {
-      id: 1,
-      img: 'assets/images/nilabh.jpeg',
-      icon: 'fa fa-star',
-      name: 'Nilabh Sahu',
-      title: 'President',
-      shortdesc:
-        'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantiuto-tam rem aperiam eaque ipsa quae inventore',
-    },
-    {
-      id: 2,
-      img: 'assets/images/dewang.jpg',
-      icon: 'fa fa-graduation-cap',
-      name: 'Dewang Sharma',
-      title: 'Senior Vice-Chairperson',
-      shortdesc:
-        'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantiuto-tam rem aperiam eaque ipsa quae inventore',
-    },
-    {
-      id: 3,
-      img: 'assets/images/ajay.jpg',
-      icon: 'fa fa-code',
-      name: 'Ajay Kumar',
-      title: 'Technical Head',
-      shortdesc:
-        'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantiuto-tam rem aperiam eaque ipsa quae inventore',
-    },
-    {
-      id: 4,
-      img: 'assets/images/onkar.jpg',
-      icon: 'fa fa-bug',
-      name: 'Onkar Setia',
-      title: 'Co-Technical Head',
-      shortdesc:
-        'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantiuto-tam rem aperiam eaque ipsa quae inventore',
-    },
-    {
-      id: 5,
-      img: 'assets/images/keshar.jpeg',
-      icon: 'fa fa-calendar-check-o',
-      name: 'Keshar Jha',
-      title: 'Events Head',
-      shortdesc:
-        'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantiuto-tam rem aperiam eaque ipsa quae inventore',
-    },
-    {
-      id: 6,
-      img: 'assets/images/amit.jpeg',
-      icon: 'fa fa-user',
-      name: 'Amit Shukla',
-      title: 'Junior Vice-Chairperson',
-      shortdesc:
-        'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantiuto-tam rem aperiam eaque ipsa quae inventore',
-    },
-    {
-      id: 7,
-      img: 'assets/images/raj.jpg',
-      icon: 'fa fa-handshake-o',
-      name: 'Raj Patel',
-      title: 'PR & Outreach Head',
-      shortdesc:
-        'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantiuto-tam rem aperiam eaque ipsa quae inventore',
-    },
-    {
-      id: 8,
-      img: 'assets/images/kush.jpeg',
-      icon: 'fa fa-desktop',
-      name: 'Kush Sinha',
-      title: 'Design & Branding Head',
-      shortdesc:
-        'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantiuto-tam rem aperiam eaque ipsa quae inventore',
-    },
-    {
-      id: 9,
-      img: 'assets/images/nimisha.jpg',
-      icon: 'fa fa-cart-plus',
-      name: 'Nimisha Jha',
-      title: 'Marketing Head',
-      shortdesc:
-        'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantiuto-tam rem aperiam eaque ipsa quae inventore',
-    },
-    {
-      id: 10,
-      img: 'assets/images/vanshika.jpg',
-      icon: 'fa fa-bullhorn',
-      name: 'Vanshika Singh',
-      title: 'Social Media Head',
-      shortdesc:
-        'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantiuto-tam rem aperiam eaque ipsa quae inventore',
-    },
-  ];
+  // serviceBlock = [
+  //   {
+  //     id: 1,
+  //     img: 'assets/images/nilabh.jpeg',
+  //     icon: 'fa fa-star',
+  //     name: 'Nilabh Sahu',
+  //     title: 'President',
+  //     shortdesc:
+  //       'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantiuto-tam rem aperiam eaque ipsa quae inventore',
+  //   },
+  //   {
+  //     id: 2,
+  //     img: 'assets/images/dewang.jpg',
+  //     icon: 'fa fa-graduation-cap',
+  //     name: 'Dewang Sharma',
+  //     title: 'Senior Vice-Chairperson',
+  //     shortdesc:
+  //       'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantiuto-tam rem aperiam eaque ipsa quae inventore',
+  //   },
+  //   {
+  //     id: 3,
+  //     img: 'assets/images/ajay.jpg',
+  //     icon: 'fa fa-code',
+  //     name: 'Ajay Kumar',
+  //     title: 'Technical Head',
+  //     shortdesc:
+  //       'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantiuto-tam rem aperiam eaque ipsa quae inventore',
+  //   },
+  //   {
+  //     id: 4,
+  //     img: 'assets/images/onkar.jpg',
+  //     icon: 'fa fa-bug',
+  //     name: 'Onkar Setia',
+  //     title: 'Co-Technical Head',
+  //     shortdesc:
+  //       'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantiuto-tam rem aperiam eaque ipsa quae inventore',
+  //   },
+  //   {
+  //     id: 5,
+  //     img: 'assets/images/keshar.jpeg',
+  //     icon: 'fa fa-calendar-check-o',
+  //     name: 'Keshar Jha',
+  //     title: 'Events Head',
+  //     shortdesc:
+  //       'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantiuto-tam rem aperiam eaque ipsa quae inventore',
+  //   },
+  //   {
+  //     id: 6,
+  //     img: 'assets/images/amit.jpeg',
+  //     icon: 'fa fa-user',
+  //     name: 'Amit Shukla',
+  //     title: 'Junior Vice-Chairperson',
+  //     shortdesc:
+  //       'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantiuto-tam rem aperiam eaque ipsa quae inventore',
+  //   },
+  //   {
+  //     id: 7,
+  //     img: 'assets/images/raj.jpg',
+  //     icon: 'fa fa-handshake-o',
+  //     name: 'Raj Patel',
+  //     title: 'PR & Outreach Head',
+  //     shortdesc:
+  //       'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantiuto-tam rem aperiam eaque ipsa quae inventore',
+  //   },
+  //   {
+  //     id: 8,
+  //     img: 'assets/images/kush.jpeg',
+  //     icon: 'fa fa-desktop',
+  //     name: 'Kush Sinha',
+  //     title: 'Design & Branding Head',
+  //     shortdesc:
+  //       'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantiuto-tam rem aperiam eaque ipsa quae inventore',
+  //   },
+  //   {
+  //     id: 9,
+  //     img: 'assets/images/nimisha.jpg',
+  //     icon: 'fa fa-cart-plus',
+  //     name: 'Nimisha Jha',
+  //     title: 'Marketing Head',
+  //     shortdesc:
+  //       'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantiuto-tam rem aperiam eaque ipsa quae inventore',
+  //   },
+  //   {
+  //     id: 10,
+  //     img: 'assets/images/vanshika.jpg',
+  //     icon: 'fa fa-bullhorn',
+  //     name: 'Vanshika Singh',
+  //     title: 'Social Media Head',
+  //     shortdesc:
+  //       'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantiuto-tam rem aperiam eaque ipsa quae inventore',
+  //   },
+  // ];
 }
